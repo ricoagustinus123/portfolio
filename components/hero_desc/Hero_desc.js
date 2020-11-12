e226a5dc2e9d44964d1fe3b2b7b8d0c092d84281
ricoagustinus123 <rico_a.m@outlook.com>
2020-11-13 06:22:03 +0700

    portfolio 12112020

---
+++ components/hero_desc/Hero_desc.js
@@ -0,0 +1,25 @@
+const HeroDesc = () => {
+    return `
+    <section class="hero-desc">
+        <h1 class="hero-desc-title">IT BERRIES</h1>
+        
+        <p class="hero-desc-isi">Nulla in velit a metus rhoncus tempus. 
+        Nulla congue nulla vel sem varius finibus. 
+        Sed ornare sit amet lorem sed viverra. 
+        In vel urna quis libero viverra facilisis ut ac est. 
+        Morbi commodo, eros in dignissim tempus, 
+        lacus odio rutrum augue, in semper sem magna quis tellus. 
+        Etiam enim erat, suscipit eu semper a, dictum sit amet elit. 
+        Nunc egestas nisi eget enim gravida facilisis. 
+        Pellentesque laoreet varius turpis vel pharetra. Ut ante justo, 
+        consequat vitae elementum tempor, accumsan nec eros.
+       
+        
+        </p>
+        <a class="read-more">Read More</a>
+      
+    </section>
+    
+    `
+}
+export default HeroDesc
--- updater.js
+++ updater.js
@@ -1,10 +1,12 @@
 import Navbar from './components/navbar/Navbar.js'
 import Hero from './components/hero/Hero.js'
+import HeroDesc from './components/hero_desc/Hero_desc.js'
 window.updater = () => {
 
     document.getElementById('root').innerHTML +=
     `${Navbar()}
     ${Hero()}
+    ${HeroDesc()}
     `;
 };
 
